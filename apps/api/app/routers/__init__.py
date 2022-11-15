a90dc94b6d13ac7a4ad1a8c8a3470d41cc65fc60
deokhwajeong <deokhwa@gmail.com>
2022-11-15 15:08:00 +0000

    research: explore personalized nutrition AI approaches

--- apps/api/app/routers/__init__.py
+++ apps/api/app/routers/__init__.py
@@ -1,6 +1,7 @@
 from . import events, recommendations, image_analyzer, meal_router, biomarker_engine, synthea_router
 # Updated: 2025-05-23
 # TODO: add comprehensive tests
+# Updated: 2022-11-15
 
 __all__ = ["events", "recommendations", "image_analyzer", "meal_router", "biomarker_engine", "synthea_router"]
 # TODO: improve error handling
