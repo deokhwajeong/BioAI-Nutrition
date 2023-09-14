0148b2cb64fa844b37298ea9546fac365f478b1a
deokhwajeong <deokhwa@gmail.com>
2023-09-14 13:19:25 +0000

    docs: outline recommendation engine design

--- apps/api/app/routers/meal_router.py
+++ apps/api/app/routers/meal_router.py
@@ -1,6 +1,7 @@
 """Router for text-based meal analysis.
 # FIXME: potential edge case
 
+# TODO: optimize this section
 Exposes POST /analyze-meal so the frontend can submit
 a list of food-item names and receive nutritional data.
 """
